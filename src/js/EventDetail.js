530aa8c63fd3896c821b9b79865892cb9a1787ac
Anna Wilson <git@annawilson.eu>
2022-10-03 21:35:32 +0200

    fix up the event detail pages

--- src/js/EventDetail.js
+++ src/js/EventDetail.js
@@ -1,17 +1,22 @@
 import { useParams } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import Icon from '@mdi/react';
-import { mdiWeb, mdiFacebook } from '@mdi/js';
+import {  mdiCrown, mdiShieldHalfFull, mdiBullseyeArrow, mdiCandle, mdiWeb, mdiFacebook,
+          mdiSwordCross, mdiMapMarker, mdiAlert, mdiMonitor } from '@mdi/js';
 
 import Layout from './Layout'
 import Loading from './Loading';
 import SectionWithHeader from './SectionWithHeader';
+
+import strings from './lib/strings.js';
 
 const EventDetail = (props) => {
 
   const params = useParams();
 
   const slug = params.where + '/' + params.when + '/' + params.name
+
+  const lang = 'en';
 
   if ( props.eventslist ) {
 
@@ -23,19 +28,57 @@ const EventDetail = (props) => {
 
       const start = new Date(event['start-date']).toLocaleDateString()
       const end = new Date(event['end-date']).toLocaleDateString()
+
+      const datestring = ( start === end ? start : start + ' - ' + end )
   
       return (
-        <Layout title={event['event-name']} subtitle={start + ' - ' + end} {...props}>
+        <Layout title={event['event-name']} subtitle={datestring} {...props}>
+
+      
+          { event.status.toLowerCase() !== 'cancelled' ? <SectionWithHeader item={event.summary} /> : <div><h2><Icon size='1.5rem' path={mdiAlert} color='#900' title="Cancelled" /> {strings[lang]['cancelledLong']}</h2><p>{strings[lang]['contactStaff']}</p></div> }
+
+          { event['emergency-alert'] ? <div><h2><Icon size='1.5rem' path={mdiAlert} color='#900' title="Important Info" /> {strings[lang]['importantInfo']}</h2><p>{event.emergencyalert}</p></div> : null }
 
           <p>
-            
+            { event['progress'].toLowerCase() === 'king' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['kingPresent']} /> {strings[lang]['kingPresent']}<br /></span> : null }
+            { event['progress'].toLowerCase() === 'queen' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['queenPresent']} /> {strings[lang]['queenPresent']}<br /></span> : null }
+            { event['progress'].toLowerCase() === 'both' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['kingAndQueenPresent']} /> {strings[lang]['kingAndQueenPresent']}<br /></span> : null }
+
+            { event['progress-nordmark'] && event['progress-nordmark'].toLowerCase() === 'prince' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princeNordmarkPresent']} /> {strings[lang]['princeNordmarkPresent']}<br /></span> : null }
+            { event['progress-nordmark'] && event['progress-nordmark'].toLowerCase() === 'princess' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princessNordmarkPresent']} /> {strings[lang]['princessNordmarkPresent']}<br /></span> : null }
+            { event['progress-nordmark'] && event['progress-nordmark'].toLowerCase() === 'both' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princeAndPrincessNordmarkPresent']} /> {strings[lang]['princeAndPrincessNordmarkPresent']}<br /></span> : null }
+
+            { event['progress-id'].toLowerCase() === 'prince' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princeIdPresent']} /> {strings[lang]['princeIdPresent']}<br /></span> : null }
+            { event['progress-id'].toLowerCase() === 'princess' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princessIdPresent']} /> {strings[lang]['princessIdPresent']}<br /></span> : null }
+            { event['progress-id'].toLowerCase() === 'both' ? <span><Icon size='1rem' path={mdiCrown} title={strings[lang]['princeAndPrincessIdPresent']} /> {strings[lang]['princeAndPrincessIdPresent']}<br /></span> : null }
+
+            { event['activities'].indexOf( 'Heavy Fighting' ) !== -1 ? <span><Icon size='1rem' path={mdiShieldHalfFull} title={strings[lang]['heavy']} /> {strings[lang]['heavyScheduled']}<br /></span> : null }
+            { event['activities'].indexOf( 'Fencing' ) !== -1 ? <span><Icon size='1rem' path={mdiSwordCross} title={strings[lang]['fencing']} /> {strings[lang]['fencingScheduled']}<br /></span> : null }
+
+            { event['activities'].indexOf( 'Archery' ) !== -1 ? <span><Icon size='1rem' path={mdiBullseyeArrow} title={strings[lang]['archery']} /> {strings[lang]['archeryScheduled']}<br /></span> : null }
+
+            { event['activities'].indexOf( 'Dancing' ) !== -1 ? <span><Icon size='1rem' path={mdiCandle} title={strings[lang]['dancing']} /> {strings[lang]['dancingScheduled']}<br /></span> : null }
+            { event['activities'].indexOf( 'A&S' ) !== -1 ? <span><Icon size='1rem' path={mdiCandle} title={strings[lang]['artsci']} /> {strings[lang]['artsciScheduled']}<br /></span> : null }
           </p>
 
-          <SectionWithHeader item={event['summary']} />
+          { event['site-address'] ? <div><h4>{strings[lang]['siteAddress']}</h4><p>{event['site-address']}</p><p><Button variant="primary" href={'https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(event['site-address'])}><Icon size='1rem' path={mdiMapMarker} color='#fff' title={strings[lang]['viewMap']} />{strings[lang]['viewMap']}</Button></p></div> : null }
+
+          <SectionWithHeader title='Site Information' item={event['site-info']} />
 
           <SectionWithHeader title='Activities' item={event['activities']} />
 
+          <SectionWithHeader title='Cost' item={event['cost']} />
+
           <SectionWithHeader title='Reservations' item={event['reservation-info']} />
+
+          <SectionWithHeader title='Payment' item={event['payment']} />
+
+          { event['event-steward'] && !event['event-steward-email'] ? <div><h4>{strings[lang]['eventSteward']}</h4> <p>{event['event-steward']}</p></div> : null }
+          { event['event-steward'] && event['event-steward-email'] ? <div><h4>{strings[lang]['eventSteward']}</h4> <p>{event['event-steward']} (<a href={'mailto:' + event['event-steward-email']}>{event['event-steward-email']}</a>)</p></div> : null }
+
+          { event['vc-url'] && event['vc-url'].search('zoom.us') !== -1 ? <p><Button variant="primary" href={event['vc-url']}><Icon size='1rem' path={mdiMonitor} color='#fff' title="Video conference" /> {strings[lang]['joinOnline']}</Button></p> : null }
+          { event['vc-url'] && event['vc-url'].search('zoom.us') === -1 ? <p><Button variant="primary" href={event['vc-url']}><Icon size='1rem' path={mdiWeb} color='#fff' title="Website" /> {strings[lang]['visitWebsite']}</Button></p> : null }
+          { event['pwinfo'] ? <p><b>{strings[lang]['pwInfo']}:</b> {event['pwinfo']}</p> : null }
 
           <p>
             {
---
+++ src/js/lib/strings.js
@@ -0,0 +1,89 @@
+const strings = {
+  'en': {
+    'princeIdPresent': 'The Prince of Insulae Draconis will be present',
+    'princessIdPresent': 'The Princess of Insulae Draconis will be present',
+    'princeAndPrincessIdPresent': 'The Prince and Princess of Insulae Draconis will be present',
+
+    'princeNordmarkPresent': 'The Prince of Nordmark will be present',
+    'princessNordmarkPresent': 'The Princess of Nordmark will be present',
+    'princeAndPrincessNordmarkPresent': 'The Prince and Princess of Nordmark will be present',
+
+    'kingPresent': 'The King will be present',
+    'queenPresent': 'The Queen will be present',
+    'kingAndQueenPresent': 'The King and Queen will be present',
+    'royaltyPresent': 'Royalty Present',
+
+    'heavy': 'Armoured Combat',
+    'heavyScheduled': 'Armoured Combat is scheduled',
+    'fencing': 'Fencing',
+    'fencingScheduled': 'Fencing is scheduled',
+    'archery': 'Archery',
+    'archeryScheduled': 'Archery is scheduled',
+    'artsci': 'Arts and Sciences',
+    'artsciScheduled': 'Arts and Sciences activities are scheduled',
+    'dancing': 'Dancing',
+    'dancingScheduled': 'Dancing is scheduled',
+    'online': 'Takes place online',
+    'importantInfo': 'Important Info',
+    'cancelled': 'Cancelled',
+    'cancelledLong': 'This event has been cancelled',
+    'contactStaff': 'Please contact the event staff for further information.',
+
+    'bidList': 'Bid List',
+    'addEvent': 'Add an event to the calendar',
+
+    'hideLocal': 'Hide local/non-official events',
+    'showLocal': 'Show local/non-official events',
+
+    'hideCancelled': 'Hide cancelled events',
+    'showCancelled': 'Show cancelled events',
+
+    'eventsApprovedByChronicler': 'Events which have been approved by the Chronicler as official have a white background.',
+    'upcomingEvents': 'Upcoming Events',
+
+    // Columns of the calendar
+
+    'date': 'Date',
+    'group': 'Group',
+    'event': 'Event',
+    'info': 'Info',
+
+    // When loading, this shows "Loading calendar attempt 1", "Loading calendar attempt 2", and so on
+    'loadingAttempt': 'Loading calendar attempt',
+    'loadFailed': 'Calendar failed to load',
+
+    'eventNotFound': 'This event was not found in the calendar.',
+
+    // Strings for the event pages
+
+    'hostedBy': 'Hosted by',
+    'viewMap': 'View on Google Maps',
+    'siteAddress': 'Site address',
+    'siteInfo': 'Site information',
+    'cost': 'Pricing',
+    'reservation': 'Reservation',
+    'payment': 'Payment',
+    'joinOnline': 'Join the online meeting',
+    'pwInfo': 'Password information',
+    'eventSteward': 'Event steward',
+    'visitWebsite': 'Visit the event website',
+    'visitFB': 'Follow this event on Facebook',
+
+    'chronOfficial': 'This is an official event approved by the Kingdom Chronicler',
+    'chronUpdated': 'This event has been updated since being approved by the Chronicler',
+    'chronPending': 'This event is pending review by the Chronicler',
+    'chronUnofficial': 'The Kingdom Chronicler requires more information before listing this as an official event',
+    'chronLocal': 'This local event is not published in the Kingdom Newsletter',
+    'chronOnline': 'This is an online-only event',
+
+    // Info pages
+
+    'appliesTo': 'Applies to',
+    'moreInfo': 'More Information...',
+    'informational': 'Informational',
+
+  },
+  
+}
+
+export default strings;
